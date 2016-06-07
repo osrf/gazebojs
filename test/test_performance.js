d218c0b2a2d464552fc51d879a3e7743ea710490
no future2e1 <no.future2e1@gmail.com>
2016-06-07 15:21:34 +0200

    counting the number of msgs in 4 secs

--- test/test_performance.js
+++ test/test_performance.js
@@ -3,29 +3,23 @@ util = require('util'),
 spawn = require('child_process').spawn;
 gazebojs = require('../index');
 
-var camera_uri = 'model://camera';
-var sensor_uri = 'model://hokuyo';
-var model_uri = 'model://cube_20k';
-var kinect_uri = 'model://kinect';
-var pioneer2dx_uri = 'model://pioneer2dx'; 
-
-suite('topics', function() {
+suite('performance', function() {
 
 // This test is to see how fast can we process msgs from a topic.
-// This would be a key to replace gzbridge by gazebojs, if the performaance
+// This would be a key to replace gzbridge by gazebojs, if the performance
 // turns out to be not good we wont be aable to do it then.
 
 // Do i need advertise ?, it would allow me to set the frequency of publishing msgs.
 
-  var gzserver;
-  var gazebo;
+var gzserver;
+var gazebo;
 
-  this.timeout(5000);
+this.timeout(5000);
 
-  suiteSetup (function(done){
+suiteSetup (function(done){
 
         // console.log('suiteSetup');
-        gzserver = spawn('gzserver', ['--verbose']);
+        gzserver = spawn('gzserver', [ __dirname + '/../examples/pendulum_cam.world']);
         gzserver.on('data', (data) => { console.log('gz: ' + data) })
         // give a second for gzserver to come up
         setTimeout(()=> {
@@ -36,35 +30,26 @@ suite('topics', function() {
         }, 100);
     });
 
-// TODO
-// the test on the top ALWAYS fails.
-test('dummy test', function(done) {
-    gazebo.sim.spawn(sensor_uri, 'hokuyo');
-    gazebo.subscribe('gazebo.msgs.Visual', '~/visual', function(e,d){
-        console.log(d);
-        assert(d.name != '');
-        gazebo.unsubscribe('~/visual');
-        done();
+  // This Warrning appears in gzserver after sth like 5 minutes of running: [Wrn] [Publisher.cc:140] 
+  // Queue limit reached for topic /gazebo/default/pose/local/info, deleting message. This warning is printed only once
+
+  // TODO: a read test has to be done, we are just counting the number of recieved msgs on the topin in a 4sec interval.
+  // How fast can gazebojs process msgs from a certain topic.
+  test('Reciving msgs', function(done) {
+    first = true;
+    counter = 0;
+    gazebo.subscribe('gazebo.msgs.PosesStamped', '~/pose/info', function(e,d){
+        counter ++;
+        setTimeout(()=> {
+            console.log(counter)
+            gazebo.unsubscribe('~/pose/info');
+  // We do need to add some sort of a condtion.
+            done();            
+        }, 4000);
     });
-    gazebo.sim.spawn(sensor_uri, 'hokuyo');
 });
 
-// Test receiving Visual msgs on visual topic.
-test('World stats topic', function(done) {
-  var counter = 0;
-    gazebo.subscribe('gazebo.msgs.WorldStatistics', '~/world_stats', function(e,d){
-        assert(d.name != '');
-        counter++;
-        console.log(counter);
-    });
-    gazebo.sim.spawn(sensor_uri, 'hokuyo');
-    if (counter > 1000){
-        gazebo.unsubscribe('~/world_stats');
-        done();
-      }
-});
-
-suiteTeardown(function() {
+  suiteTeardown(function() {
     console.log('suiteTeardown');
     gzserver.kill('SIGHUP');
 });
