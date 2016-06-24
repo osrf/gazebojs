7967ed2b90541ac5e00c0215e7f1f79f846c8c2e
no future2e1 <no.future2e1@gmail.com>
2016-06-24 17:38:01 +0200

    merging upstream

---
+++ test/test_performance.js
@@ -0,0 +1,82 @@
+var assert = require('assert'),
+util = require('util'),
+spawn = require('child_process').spawn;
+gazebojs = require('../index');
+
+suite('performance', function() {
+
+// This test is to see how fast can we receive msgs from gzserver.
+
+var gzserver;
+var gazebo;
+
+// Currently we are testing with 8 subscribers, more than that may require 
+// a time period more than 4 seconds to avoid a (core dumped) error 
+// [because gzserver didn't have the chance to unsubscribe].
+var test_period = 4000;
+var test_period_sec = test_period /1000;
+
+this.timeout(5000+test_period);
+
+suiteSetup (function(done){
+
+        // console.log('suiteSetup');
+        gzserver = spawn('gzserver', [ __dirname + '/../examples/pendulum_cam.world']);
+        gzserver.on('data', (data) => { console.log('gz: ' + data) })
+        // give a second for gzserver to come up
+        setTimeout(()=> {
+            gazebo = new gazebojs.Gazebo();
+            gazebo.proc = gzserver
+            console.log('sim pid: ' + gazebo.proc.pid)
+            done();
+        }, 100);
+    });
+
+    // How fast can gazebojs process msgs from a certain topic.
+    test('Reciving msgs', function(done) {
+        first = true;
+        counter = 0;
+        gazebo.subscribe('gazebo.msgs.PosesStamped', '~/pose/info', function(e,d){
+            counter ++;
+        });
+        gazebo.subscribe('gazebo.msgs.PosesStamped', '~/pose/info', function(e,d){
+            counter ++;
+        });
+        gazebo.subscribe('gazebo.msgs.PosesStamped', '~/pose/info', function(e,d){
+            counter ++;
+        });
+        gazebo.subscribe('gazebo.msgs.PosesStamped', '~/pose/info', function(e,d){
+            counter ++;
+        });
+        gazebo.subscribe('gazebo.msgs.PosesStamped', '~/pose/info', function(e,d){
+            counter ++;
+        });
+        gazebo.subscribe('gazebo.msgs.PosesStamped', '~/pose/info', function(e,d){
+            counter ++;
+        });
+        gazebo.subscribe('gazebo.msgs.PosesStamped', '~/pose/info', function(e,d){
+            counter ++;
+        });
+        gazebo.subscribe('gazebo.msgs.PosesStamped', '~/pose/info', function(e,d){
+            counter ++;
+            setTimeout(()=> {
+              var rate = counter/test_period_sec;
+                console.log(counter + ' messages received in ' + test_period_sec + ' seconds, ' + rate +' messages/sec')
+                gazebo.unsubscribe('~/pose/info');
+                // We would consider this a minimum rate for now, could be changed later.
+                if(rate < 450){
+                  assert.fail(rate,450,'msgs reciving rate too slow','<');
+                }
+                else{
+                done();
+                }            
+            }, test_period);
+        });
+    });
+
+      suiteTeardown(function() {
+        console.log('suiteTeardown');
+        gzserver.kill('SIGHUP');
+      });
+
+});
--- test/test_topics.js
+++ test/test_topics.js
@@ -6,8 +6,20 @@ gazebojs = require('../index');
 var camera_uri = 'model://camera';
 var sensor_uri = 'model://hokuyo';
 var model_uri = 'model://cube_20k';
+var kinect_uri = 'model://kinect';
+var pioneer2dx_uri = 'model://pioneer2dx'; 
 
 suite('topics', function() {
+
+    // Topics that are not tested yet.
+    // "~/link";
+    // "~/physics";
+    // "~/material";
+    // "~/heightmap_data";
+    // "~/model/modify";
+    // "~/roads";
+    // "~/factory/light";
+    // "~/light/modify";
 
     var gzserver;
     var gazebo;
@@ -28,53 +40,92 @@ suite('topics', function() {
         }, 100);
     });
 
-// Test receiving Sensor msgs on sensor topic.
-test('sensor topic', function(done) {
-    gazebo.subscribe('gazebo.msgs.Sensor', '~/sensor', function(e,d){
-        console.log(d);
-        assert(d.name == 'laser');
-        gazebo.unsubscribe('~/sensor');
-        done();
+    // Test receiving Joint msgs on joint topic.
+    test('joint topic', function(done) {
+        gazebo.subscribe('gazebo.msgs.Joint', '~/joint', function(e,d){
+            assert(d.name != '');
+            gazebo.unsubscribe('~/joint');
+            done();
+        });
+        setTimeout(()=>{
+            gazebo.sim.spawn(pioneer2dx_uri, 'pioneer2dx');
+        },3500)     
     });
-    gazebo.sim.spawn(sensor_uri, 'hokuyo');
-});
 
-// Test receiving Visual msgs on visual topic.
-test('visual topic', function(done) {
-    gazebo.subscribe('gazebo.msgs.Visual', '~/visual', function(e,d){
-        console.log(d);
-        assert(d.name == 'hokuyo');
-        gazebo.unsubscribe('~/visual');
-        done();
+    // Test receiving PoseStamped msgs on pose/info.
+    test('pose/info topic', function(done) {
+        gazebo.subscribe('gazebo.msgs.PoseStamped', '~/pose/info', function(e,d){
+            assert(d.name != '');
+            gazebo.unsubscribe('~/pose/info');
+            done();
+        });
+        gazebo.sim.spawn(pioneer2dx_uri, 'pioneer2dx');
     });
-    gazebo.sim.spawn(sensor_uri, 'hokuyo');
-});
 
-// Test receiving Camera msgs on camera topic.
-// test('camera topic', function(done) {
-//     gazebo.subscribe('gazebo.msgs.Camera', '~/camera', function(e,d){
-//         console.log(d);
-//         assert(d.name != '');
-//         gazebo.unsubscribe('~/camera');
-//         done();
-//     });
-//     gazebo.sim.spawn(camera_uri, 'camera');
-// });
+    // Test receiving WorldControl msgs on joint world_control topic.
+    test('world/control topic', function(done) {
+        gazebo.subscribe('gazebo.msgs.WorldControl', '~/world_control', function(e,d){
+            assert(d.pause);
+            gazebo.unsubscribe('~/world_control');
+            gazebo.publish("gazebo.msgs.WorldControl",  "~/world_control", {pause:false});
+            done();
+        });
+        gazebo.publish("gazebo.msgs.WorldControl",  "~/world_control", {pause:true});
+    });
 
-// // Test receiving Model msgs on model topic.
-// test('model info topic', function(done) {
-//     gazebo.subscribe('gazebo.msgs.Model', '~/model', function(e,d){
-//         console.log(d);
-//         assert(d.name != '');
-//         gazebo.unsubscribe('~/model');
-//         done();
-//     });
-//     gazebo.sim.spawn(sensor_uri, 'cube_20k');
-// });
+    // Test receiving factory msgs on factory topic.
+    test('factory topic', function(done) {
+        gazebo.subscribe('gazebo.msgs.Factory', '~/factory', function(e,d){
+            assert(d.name != '');
+            gazebo.unsubscribe('~/factory');
+            done();
+        });
+        gazebo.sim.spawn(pioneer2dx_uri, 'pioneer2dx');
+    });
 
-suiteTeardown(function() {
-    console.log('suiteTeardown');
-    gzserver.kill('SIGHUP');
-});
+    // Test receiving Visual msgs on visual topic.
+    test('visual topic', function(done) {
+        gazebo.subscribe('gazebo.msgs.Visual', '~/visual', function(e,d){
+            assert(d.name != '');
+            gazebo.unsubscribe('~/visual');
+            done();
+        });
+        gazebo.sim.spawn(sensor_uri, 'hokuyo');
+    });
+
+    // Test receiving Sensor msgs on sensor topic.
+    test('sensor topic', function(done) {
+        gazebo.subscribe('gazebo.msgs.Sensor', '~/sensor', function(e,d){
+            assert(d.name != '');
+            gazebo.unsubscribe('~/sensor');
+            done();
+        });
+        gazebo.sim.spawn(kinect_uri, 'kinect');
+    });
+
+    // Test receiving Model msgs on model topic.
+    test('model info topic', function(done) {
+        gazebo.subscribe('gazebo.msgs.Model', '~/model/info', function(e,d){
+            assert(d.name != '');
+            gazebo.unsubscribe('~/model/info');
+            done();
+        });
+        gazebo.sim.spawn(model_uri, 'cube_20k');
+    });
+
+    // Test response and request topics.
+    test('request and response topics test', function(done) {
+       gazebo.subscribe('gazebo.msgs.Response', '~/response', function(e,d){
+            assert(d.response === 'success' && d.request === 'entity_delete');
+            gazebo.unsubscribe('~/response');
+            done();
+        });
+       gazebo.deleteEntity('cube_20k');
+   });
+
+    suiteTeardown(function() {
+        console.log('suiteTeardown');
+        gzserver.kill('SIGHUP');
+    });
 
 });
